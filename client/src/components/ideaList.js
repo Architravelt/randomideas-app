78c6fd994abeadd86651b85aebb05a20340c9ebd
Clare Amos <clare.amos@gmail.com>
2024-08-06 11:03:10 +0100

    API service - get ideas

--- client/src/components/ideaList.js
+++ client/src/components/ideaList.js
@@ -1,22 +1,10 @@
+import IdeasApi from '../services/ideasApi';
+
 class IdeaList {
   constructor() {
     this._ideaListEl = document.querySelector('#idea-list');
-    this._ideas = [
-      {
-        id: 1,
-        text: 'Idea 1',
-        tag: 'Business',
-        username: 'Jessica',
-        date: '5/8/2024',
-      },
-      {
-        id: 2,
-        text: 'Idea 2',
-        tag: 'Technology',
-        username: 'Jareth',
-        date: '5/8/2024',
-      },
-    ];
+    this._ideas = [];
+    this.getIdeas();
     this._validTags = new Set();
     this._validTags.add('technology');
     this._validTags.add('software');
@@ -24,6 +12,17 @@ class IdeaList {
     this._validTags.add('education');
     this._validTags.add('health');
     this._validTags.add('inventions');
+  }
+
+  async getIdeas() {
+    try {
+      const response = await IdeasApi.getIdeas();
+      this._ideas = response.data.data;
+      this.render();
+      console.log(this._ideas);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   getTagClass(tag) {
